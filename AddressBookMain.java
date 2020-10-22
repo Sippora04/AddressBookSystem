fef1dc212e3410ddf4fe8159b8ffb0426a256da4
Sippora04 <sippora.toppo95@gmail.com>
2020-10-23 03:18:17 +0530

    Added: Contacts details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class AddressBookMain
-{	public static void main(String[] args)
-	{
-		System.out.println("Welcome to Address Book ");
+{
+	private static Scanner sc = new Scanner(System.in);;
+
+	public static void main(String args[])
+	{	
+		System.out.print("Enter First Name:");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name:");
+        String lastName = sc.nextLine();
+        System.out.print("Enter Address:");
+        String address = sc.nextLine();
+        System.out.print("Enter City:");
+        String city = sc.nextLine();
+        System.out.print("Enter state:");
+        String state = sc.nextLine();
+        System.out.print("Enter Pincode/Zipcode:");
+        int pinCode = sc.nextInt();
+        System.out.print("Enter Phone No:");
+        int phoneNumber = sc.nextInt();
+        System.out.print("Enter email Id:");
+        String emailId = sc.nextLine();
 	}
 }

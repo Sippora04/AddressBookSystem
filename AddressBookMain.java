8b87794ccc488e7f458d0e99b811ab92db35ed49
Sippora04 <sippora.toppo95@gmail.com>
2020-10-25 13:18:37 +0530

    Modified: changes in code

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,26 +1,42 @@
 import java.util.Scanner;
 
-public class AddressBookMain
-{
-	private static Scanner sc = new Scanner(System.in);;
-
-	public static void main(String args[])
-	{	
-		System.out.print("Enter First Name:");
-        String firstName = sc.nextLine();
-        System.out.print("Enter Last Name:");
-        String lastName = sc.nextLine();
-        System.out.print("Enter Address:");
-        String address = sc.nextLine();
-        System.out.print("Enter City:");
-        String city = sc.nextLine();
-        System.out.print("Enter state:");
-        String state = sc.nextLine();
-        System.out.print("Enter Pincode/Zipcode:");
-        int pinCode = sc.nextInt();
-        System.out.print("Enter Phone No:");
-        int phoneNumber = sc.nextInt();
-        System.out.print("Enter email Id:");
-        String emailId = sc.nextLine();
+public class AddressBookMain {
+	public Contacts createContact() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter First Name:");
+		String firstName = sc.next();
+		
+		System.out.println("Enter Last Name:");
+		String lastName = sc.next();
+		
+		System.out.println("Enter address:");
+		String address = sc.next();
+		
+		System.out.println("Enter city:");
+		String city = sc.next();
+		
+		System.out.println("Enter state:");
+		String state = sc.next();
+		
+		System.out.println("Enter zip code:");
+		int zip = sc.nextInt();
+		
+		System.out.println("Enter phone No.:");
+		String phoneNumber = sc.next();
+		
+		System.out.println("Enter email address:");
+		String email = sc.next();
+		sc.close();
+		Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		
+		return contact;		
+	}
+	
+	public static void main(String[] args) {
+		
+		AddressBookMain addressBookObject = new AddressBookMain();		
+		Contacts contact = addressBookObject.createContact();
+		System.out.println("New Contact Created: " + "\n"+contact.toString());		
 	}
 }

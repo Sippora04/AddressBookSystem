bf2fcb0bf2905ab33a5987282c1b1d5aed8742bd
Sippora04 <sippora.toppo95@gmail.com>
2020-10-25 14:30:37 +0530

    Added: New Contact details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,26 +1,67 @@
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
-public class AddressBookMain
-{
-	private static Scanner sc = new Scanner(System.in);;
+public class AddressBookMain {
+	
+	private Map<String, Contacts> addressBook = new TreeMap<String, Contacts>();
 
-	public static void main(String args[])
-	{	
-		System.out.print("Enter First Name:");
-        String firstName = sc.nextLine();
-        System.out.print("Enter Last Name:");
-        String lastName = sc.nextLine();
-        System.out.print("Enter Address:");
-        String address = sc.nextLine();
-        System.out.print("Enter City:");
-        String city = sc.nextLine();
-        System.out.print("Enter state:");
-        String state = sc.nextLine();
-        System.out.print("Enter Pincode/Zipcode:");
-        int pinCode = sc.nextInt();
-        System.out.print("Enter Phone No:");
-        int phoneNumber = sc.nextInt();
-        System.out.print("Enter email Id:");
-        String emailId = sc.nextLine();
+	public void addContactToAddressBook(Contacts contact) {
+		addressBook.put(contact.getFullName(), contact);
+	}
+	
+	public void displayAddressBook() {
+		for(Map.Entry<String,Contacts> contact : addressBook.entrySet()) {
+			System.out.println(contact.getValue());
+		}
+	}
+	
+	public Contacts createContact() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter First Name:");
+		String firstName = sc.next();
+		
+		System.out.println("Enter Last Name:");
+		String lastName = sc.next();
+		
+		System.out.println("Enter address:");
+		String address = sc.next();
+		
+		System.out.println("Enter city:");
+		String city = sc.next();
+		
+		System.out.println("Enter state:");
+		String state = sc.next();
+		
+		System.out.println("Enter zip code:");
+		int zip = sc.nextInt();
+		
+		System.out.println("Enter phone No.:");
+		String phoneNumber = sc.next();
+		
+		System.out.println("Enter email address:");
+		String email = sc.next();
+		sc.close();
+		Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		
+		return contact;		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		AddressBookMain addressBookObject = new AddressBookMain();		
+		System.out.println("Add new Contact(Y/N): ");
+		String select = sc.next();
+		switch(select) {
+		case "Y": Contacts contact = addressBookObject.createContact();
+				  addressBookObject.addContactToAddressBook(contact);
+				  break;
+		case "N": System.out.println("Cancel to add Contact");
+				  break;
+				  default: System.out.println("Invalid choice");
+		}
+		addressBookObject.displayAddressBook();
+		sc.close();
 	}
 }
